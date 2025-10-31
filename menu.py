fc9f5b03ae6217777645fb37d991caa4cccc57df
computadorana27-dot <computador.ana27@gmail.com>
2025-10-31 09:05:24 -0300

    Importação do Gerenciador e definição de funções iniciais

---
+++ menu.py
@@ -0,0 +1,10 @@
+from Banco import Gerenciador
+
+def mostrar_titulo():
+    """
+    Mostra o título do sistema.
+    """
+    print("=" * 40)
+    print("   🐶 SISTEMA DE BANCO🐱")
+    print("=" * 40)
+    print()
