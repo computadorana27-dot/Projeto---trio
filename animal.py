81e9f5ee75a9c092703dc07b994605445f97b495
Andrea <andreanogueira778@gmail.com>
2025-10-31 09:05:46 -0600

    adicionei a classe animal

---
+++ animal.py
@@ -0,0 +1,33 @@
+import json
+
+# ----- CLASSES -----
+class Animal:
+    """
+    Classe base para todos os animais.
+    """
+    def __init__(self, nome, especie, raca, idade, dono):
+        self.nome = nome
+        self.especie = especie
+        self.raca = raca
+        self.idade = idade
+        self.dono = dono
+
+    def to_dict(self):
+        """
+        Converte o objeto em dicionário para salvar em JSON.
+        """
+        return {
+            "tipo": "Animal",
+            "nome": self.nome,
+            "especie": self.especie,
+            "raca": self.raca,
+            "idade": self.idade,
+            "dono": self.dono
+        }
+
+    def __str__(self):
+        """
+        Retorna uma representação legível do animal.
+        """
+        return f"{self.especie} - Nome: {self.nome}, Raça: {self.raca}, Idade: {self.idade}, Dono: {self.dono}"
+
